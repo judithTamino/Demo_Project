ae438a19f75b63d63f7c45499219284dc0b95e21
judithTamino <55831729+judithTamino@users.noreply.github.com>
2022-07-02 23:49:59 -0700

    New change!!!

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -13,7 +13,7 @@ namespace DemoProject
             Calc calc = new Calc();
             string print = calc.Check(15, 15).ToString();
             Console.WriteLine(print);
-
+            Console.WriteLine("hhhhhh");
             Console.Read();
         }
     }
