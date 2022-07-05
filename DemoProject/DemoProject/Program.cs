84a8399061d0500f47e39b23d15613215e4a899a
judithTamino <55831729+judithTamino@users.noreply.github.com>
2022-07-05 09:09:49 -0700

    commit N.7d3fac25-e47f-4c84-8b3f-9911f373e208

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -7,6 +7,7 @@ namespace DemoProject
         {
             Calc calc = new Calc();
             string print = calc.Check(15, 15).ToString();
+            Console.WriteLine(print);
       
             Console.Read();
         }
