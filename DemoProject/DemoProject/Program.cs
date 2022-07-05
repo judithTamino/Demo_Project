d408fcc8829b1cdd6585550570b8214fc24bfe38
judithTamino <55831729+judithTamino@users.noreply.github.com>
2022-07-05 08:16:05 -0700

    commit N.298c5535-dc9b-4432-b277-a396b5d2bd1e

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -14,7 +14,7 @@ namespace DemoProject
             string print = calc.Check(15, 15).ToString();
             string print2 = calc.Check(10, 10).ToString();
 
-            Console.WriteLine("Judith Tamino");
+            Console.WriteLine("Judith");
 
             Console.WriteLine(print + print2);
             Console.Read();
