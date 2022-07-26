b243dd1fb169e0d02b9e29d060f0a84baccf3dfe
judithTamino <55831729+judithTamino@users.noreply.github.com>
2022-07-26 09:14:57 -0700

    commit

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -6,12 +6,11 @@ namespace DemoProject
         static void Main(string[] args)
         {
             Calc calc = new Calc();
-            string print = calc.Check(15, 15).ToString();
-            string print2 = calc.Check(15, 5).ToString();
+            string print = calc.Check(10, 15).ToString();
+            string print2 = calc.Check(10, 10).ToString();
 
             Console.WriteLine(print);
-           
-      
+            Console.WriteLine(print2);
             Console.Read();
         }
     }
